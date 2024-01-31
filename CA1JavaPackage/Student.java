2c58dad6f4637d8fb16c5c2bed707f6087cff4be
Angus Friel <angus.friel.1986@gmail.com>
2024-01-31 16:59:21 +0000

    Add variables for student object, setters & getters to allow external code to set & retrieve the values in the variables

--- CA1JavaPackage/Student.java
+++ CA1JavaPackage/Student.java
@@ -6,6 +6,61 @@
 //Declaring package
 package CA1JavaPackage;
 
+// This line declares a public class named Student. Being public, it can be accessed from other classes.
 public class Student {
+
+        // Defining private variables. These variables are private, meaning they can only be accessed within the Student class.
+        private String firstName;
+        private String lastName;
+        private int classNumber;
+        private String studentNumber;
+    
+        // Setters - These methods allow external code to set the values of the variables. This is part of the encapsulation principle in OOP
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+    
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    
+        public void setClassNumber(int classNumber) {
+            this.classNumber = classNumber;
+        }
+    
+        public void setStudentNumber(String studentNumber) {
+            this.studentNumber = studentNumber;
+        }
+    
+        // Getters - These methods allow external code to retrieve the values of the variables. Again, this is an implementation of encapsulation.
+        public String getFirstName() {
+            return firstName;
+        }
+    
+        public String getLastName() {
+            return lastName;
+        }
+    
+        public int getClassNumber() {
+            return classNumber;
+        }
+    
+        public String getStudentNumber() {
+            return studentNumber;
+        }
+    
+        // This is a public method that determines the workload of a student based on the classNumber
+        public String determineWorkload() {
+            if (classNumber == 1) {
+                return "very light";
+            } else if (classNumber == 2) {
+                return "light";
+            } else if (classNumber >= 3 && classNumber <= 5) {
+                return "Part-Time";
+            } else if (classNumber >= 6) {
+                return "Full-Time";
+            }
+            return "Unknown";
+        }
     
 }
