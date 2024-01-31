8fdbe26ca3f29b52c10558c4b9d35ae3bc41f66e
Angus Friel <angus.friel.1986@gmail.com>
2024-01-31 16:48:31 +0000

    Add switch statement to handle user choice for mentu items

--- CA1JavaPackage/StudentProcessor.java
+++ CA1JavaPackage/StudentProcessor.java
@@ -48,7 +48,79 @@ public class StudentProcessor{
             System.out.println();
             scanner.nextLine();
 
-        }
-    }
+            / Defining a variable that will be used to return the user to the main menu
+            String returnToMenuChoice;
 
-} 
+            switch (choice) {
+                case 1:
+                    // Call the method to add new student data
+                    studentService.addNewStudentData(scanner);
+                    studentService.processAndWriteData();
+                    break;
+                case 2:
+                        // Check file format using fileService
+                        boolean isValidFormat = fileService.isValidStudentFileFormat("students.txt");
+                        if (isValidFormat) {
+                            // Prints a message in green stating format uis correct
+                            System.out.println("\u001B[32mstudents.txt is in the correct format.");
+                            System.out.println();
+                        } else {
+                            //  Displays an error message in red that the format is incorrect
+                            System.out.println("\u001B[31mstudents.txt is not in the correct format.");
+                            System.out.println();
+                            // Display invalid lines
+                            fileService.displayInvalidLines("students.txt");
+                        }
+                        // Ask the user if they want to return to the main menu
+                        System.out.println();
+                        System.out.print("\\u001B[32mDo you want to return to the main menu? (yes/no): ");
+                        System.out.println();
+                        //Initializing the variable returnToMenuChoice so the user can return to the main menu
+                        returnToMenuChoice = scanner.nextLine();
+                        // If the user selects Y or YES the return to the main menu, if the user selects N ot NO then program ends
+                        if ("Y".equalsIgnoreCase(returnToMenuChoice) || "YES".equalsIgnoreCase(returnToMenuChoice)) {
+                            // Clear the console and return to the main menu
+                            ConsoleClear.clearConsole();
+                        }
+                        break;
+                case 3:
+                    // Display status.txt using fileService
+                    fileService.displayStatusFile();
+                    System.out.println();
+                    // Ask the user if they want to return to the main menu
+                    System.out.print("\\u001B[32mDo you want to return to the main menu? (yes/no): ");
+                    //Initializing the variable returnToMenuChoice so the user can return to the main menu
+                    returnToMenuChoice = scanner.nextLine();
+                    // If the user selects Y or YES the return to the main menu, if the user selects N ot NO then program ends
+                    if ("Y".equalsIgnoreCase(returnToMenuChoice) || "YES".equalsIgnoreCase(returnToMenuChoice)) {
+                        // Clear the console and return to the main menu
+                        ConsoleClear.clearConsole();
+                        break;
+                    }
+                case 4:
+                    // Displays the students.txt file
+                    fileService.displayStudentsFile();
+                    // Ask the user if they want to return to the main menu
+                    System.out.println();
+                    System.out.print("\\u001B[32mDo you want to return to the main menu? (yes/no): ");
+                    //Initializing the variable returnToMenuChoice so the user can return to the main menu
+                    returnToMenuChoice = scanner.nextLine();
+                    // If the user selects Y or YES the return to the main menu, if the user selects N ot NO then program ends
+                    if ("Y".equalsIgnoreCase(returnToMenuChoice) || "YES".equalsIgnoreCase(returnToMenuChoice)) {
+                        // Clear the console and return to the main menu
+                        ConsoleClear.clearConsole();
+                        break;
+                    }
+                case 5:
+                    // end the program 
+                    exit = true;
+                    break;
+                default:
+                    // displays an error message if the user selects an invalid choice from the menu 
+                    System.out.println("\u001B[31mInvalid choice. Please try again.");
+            }
+        }
+
+    }
+}
+
